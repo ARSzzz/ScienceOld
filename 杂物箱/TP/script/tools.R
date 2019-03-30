2d0fa07f38f766e5ee05c77967250421cb82fc4b
clumsykun <464564598@qq.com>
2019-03-30 20:01:03 +0800

    remove trash

--- 杂物箱/TP/script/tools.R
+++
@@ -1,6 +0,0 @@
-for (i in 1:nrow(test)) {
-  x.p = as.integer(strsplit(test[i, 'ZYTS.data'][[1]], split = ',')[[1]])
-  y.p = as.integer(strsplit(test[i, 'ZFY.data'][[1]], split = ',')[[1]])
-  plot(x.p, y.p)
-  
-}
